8e8ef8a8e8ef90307e58064dd16445b5f911680a
Leandro Santana <leandro09@unifei.edu.br>
2023-02-21 21:16:24 -0300

    att config

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-DEBUG = True
+import os
 
+
+DEBUG = True
+port = os.environ.get('PORT', 5000)
+host = '0.0.0.0'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
